1e107277dbb6760ed5966894c66af7ea9dd9faa5
unknown <271920984@qq.com>
2023-11-18 17:44:06 +0800

    update: add dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import apis from '@/apis/api'
+import VueRouter from 'vue-router'
+import router from '@/router/router'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
+Vue.prototype.$api = apis
+Vue.use(VueRouter)
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
